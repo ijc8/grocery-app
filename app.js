4d086237eb4bb47dc8d39d6909119b8d40fd2134
Ian Clester <ijc@mit.edu>
2018-09-15 15:59:14 -0400

    Switch to express, add sample data.

--- app.js
+++ app.js
@@ -1,57 +1,77 @@
-var http = require('http');
-var url = require('url');
-var Peapod = require('peapod-v3');
-var secrets = require('./secrets.json');
-var peapod = new Peapod(secrets);
+const url = require('url');
+const Peapod = require('peapod-v3');
+const secrets = require('./secrets.json');
+const peapod = new Peapod(secrets);
+const express = require('express');
+const app = express();
 
-var server = http.createServer(function(req, resp) {
-    resp.writeHead(200, {'Content-Type': 'text/html'});
-
-    var page = url.parse(req.url).pathname;
-    var split = page.split('/');
-    
-    switch (split[1]){
-	    case 'search':
-            var item = decodeURIComponent(split[2]);
-            peapod.search(item, function (err, results) {
-                resp.writeHead(200, {'Content-Type': 'text/plain'});
-                resp.write(JSON.stringify(results));
-                resp.end();
-            });
-            break;
-	    case 'add':
-            // bananas: 197476
-            var productId = parseInt(split[2]);
-            var quantity = parseInt(split[3]);
-            peapod.addToCart(productId, quantity, function(err, didSucceed) {
-                resp.writeHead(200, {'Content-Type': 'text/plain'});
-                resp.write(JSON.stringify(didSucceed));
-                resp.end();
-            });
-            break;
-	    case 'remove':
-            // probably use add to overwrite if removing < n items
-            var productId = parseInt(split[2]);
-            peapod.removeFromCart(productId, function (err, didSucceed) {
-                resp.writeHead(200, {'Content-Type': 'text/plain'});
-                resp.write(JSON.stringify(didSucceed));
-                resp.end();
-            });
-            break;
-        case 'view':
-            peapod.viewCart(function (err, results) {
-                resp.writeHead(200, {'Content-Type': 'text/plain'});
-                resp.write(JSON.stringify(results));
-                resp.end();
-            });
-            break;
-        default:
-            resp.writeHead(200, {'Content-Type': 'text/plain'});
-            resp.write('sad');
-            resp.end();
+var users = [
+    {
+        username: "ijc",
+        balance: 5.00,
+        cart: {
+            unsubmitted: [
+                {
+                    id: 197476,
+                    qty: 1,
+                }
+            ],
+            submitted: [
+                {
+                    id: 197476,
+                    qty: 4500,
+                }
+            ],
+            paid: [
+                {
+                    id: 197476,
+                    qty: 4500,
+                }
+            ]
+        },
+        past_orders: [
+            {
+                timestamp: 0,
+                cart: [
+                    {
+                        id: 197476,
+                        qty: 1999,
+                    }
+                ],
+                total: 3000000000,
+            }
+        ]
     }
+]
+
+app.get('/', (req, res) => res.send('sad'));
+
+app.get('/search/:query', (req, res) => {
+    peapod.search(req.params.query, (err, results) => {
+        res.send(JSON.stringify(results));
+    });
+});
+
+app.get('/add/:id/:qty', (req, res) => {
+    var pid = parseInt(req.params.id);
+    var qty = parseInt(req.params.qty);
+    peapod.addToCart(pid, qty, (err, didSucceed) => {
+        res.send(JSON.stringify(didSucceed));
+    });
+});
+
+app.get('/remove/:id', (req, res) => {
+    var pid = parseInt(req.params.id);
+    peapod.removeFromCart(pid, qty, (err, didSucceed) => {
+        res.send(JSON.stringify(didSucceed));
+    });
+});
+
+app.get('/view', (req, res) => {
+    peapod.viewCart(function (err, results) {
+        res.send(JSON.stringify(results));
+    });
 });
 
 var port = 8080;
-server.listen(port);
-console.log('Listening on %d', port);
+app.listen(port, () => console.log('Listening on %d', port));
